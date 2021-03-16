55d4e4dcbcd3d02f5ab127e4564babe17cd1a36d
Boris <sslonovborisss@gmail.com>
2021-03-16 06:07:39 +0300

    add

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,40 @@
+"use strict"
+
+//burger
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__list'),
+        menuItem = document.querySelectorAll('.header__link'),
+        hamburger = document.querySelector('.header__burger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('burger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('burger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+   
+  });
+
