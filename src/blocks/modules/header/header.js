f792bba09f4a3a1d5ab1f3e5c6e471d52c3c205b
Boris <sslonovborisss@gmail.com>
2021-03-23 03:49:38 +0300

    add content

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -42,10 +42,8 @@ const swiper = new Swiper('.swiper-container', {
 const swiperHome = new Swiper('.swiper-container-home', {
     // Optional parameters
     loop: true,
+    direction: 'vertical',
     effect: 'fade',
-
-
-
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
@@ -57,7 +55,6 @@ const swiperHome = new Swiper('.swiper-container-home', {
 const swiperHouses = new Swiper('.swiper-container-houses', {
     // Optional parameters
     autoHeight: true,
-    loop: true,
     effect: 'coverflow',
     spaceBetween: 10,
     // Navigation arrows
@@ -73,17 +70,17 @@ const swiperHouses = new Swiper('.swiper-container-houses', {
 
 
 $('a[href*="#"]').click(function (event) {
-	if (
-		location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		if (target.length) {
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top - 200 + "px"
-			}, 1000);
-		}
-	}
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 200 + "px"
+            }, 1000);
+        }
+    }
 });
 
 
